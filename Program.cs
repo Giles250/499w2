b5ddb9527af3cb4fa98dfdbe2ccbeb0fa5767a55
Jimmy <jimmy123good@hotmail.com>
2020-09-22 16:17:16 -0700

    Revert "new"
    This reverts commit 7d95a673ab571250de7345bcc0932a055783785d.

--- Program.cs
+++ Program.cs
@@ -2,45 +2,36 @@
 
 namespace _499w2
 {
-    interface BinaryOperator<T>
-    {
+    interface BinaryOperator<T> {
         T call(T a, T b);
     }
 
-    class Add<T> : BinaryOperator<T>
-    {
-        public T call(T a, T b)
-        {
+    class Add<T> : BinaryOperator<T> {
+        public T call(T a, T b) {
             dynamic a1 = a;
             dynamic b1 = b;
             return (T)(a1 + b1);
         }
     }
 
-    class Minus<T> : BinaryOperator<T>
-    {
-        public T call(T a, T b)
-        {
+    class Minus<T> : BinaryOperator<T> {
+        public T call(T a, T b) {
             dynamic a1 = a;
             dynamic b1 = b;
             return (T)(a1 - b1);
         }
     }
 
-    class Muliply<T> : BinaryOperator<T>
-    {
-        public T call(T a, T b)
-        {
+    class Muliply<T> : BinaryOperator<T> {
+        public T call(T a, T b) {
             dynamic a1 = a;
             dynamic b1 = b;
             return (T)(a1 * b1);
         }
     }
 
-    class Divide<T> : BinaryOperator<T>
-    {
-        public T call(T a, T b)
-        {
+    class Divide<T> : BinaryOperator<T> {
+        public T call(T a, T b) {
             dynamic a1 = a;
             dynamic b1 = b;
             return (T)(a1 / b1);
@@ -62,7 +53,7 @@ namespace _499w2
             // add test;
             Console.WriteLine("My add works: " + addOp.call(1, 2));
             Console.WriteLine("My minus(Giles) works: " + minOp.call(4, 2));
-            Console.WriteLine("My multiply(Andrew) works:" + mulOp.call(5, 5));
+            Console.WriteLine("My multiply(Andrew) works:" + mulOp.call(5,5));
             Console.WriteLine("My divide(Winter) works: " + divOp.call(16, 2));
             Console.WriteLine("Hello World!");
         }
